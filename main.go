15eff0c962570d53293a7de6eb0d8660dff01636
sweatshirt <sweatshirtblue2@gmail.com>
2025-07-16 09:45:29 +0300

    progress: with chi middleware

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
 	println("Starting app...")
 
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
 	server := &http.Server{
 		Addr: ":3000",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Hello World"))
-		}),
+		Handler: router,
 	}
 
 	println("Starting server...")
