6a34dc7447a69ce23de9fb60feea51e9c5730984
sweatshirt <sweatshirtblue2@gmail.com>
2025-07-16 09:49:33 +0300

    docs: comments for the opponents

--- main.go
+++ main.go
@@ -10,12 +10,19 @@ import (
 func main() {
 	println("Starting app...")
 
+	// creating a chi router instance
 	router := chi.NewRouter()
+	// applying the chi middleware
 	router.Use(middleware.Logger)
+
+	// defining a chi route
 	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
 
+	// creating a net/http instance
+	// using reference of the http.Server even though go implicitly dereferences the instance on each method call
+	// why you ask? ask chatgpt
 	server := &http.Server{
 		Addr: ":3000",
 		Handler: router,
@@ -24,6 +31,7 @@ func main() {
 	println("Starting server...")
 	err := server.ListenAndServe()
 
+	// explicitly panicking because well... it's go
 	if err != nil {
 		panic(err)
 	}
