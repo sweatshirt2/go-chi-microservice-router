12898f09a9d43b5fdf938715839a1761224bf171
sweatshirt <sweatshirtblue2@gmail.com>
2025-07-23 05:48:31 +0300

    redis

--- application/app.go
+++ application/app.go
@@ -4,16 +4,24 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rds *redis.Client
 }
 
 func NewApp() *App {
 	router := loadRoutes()
+  rds := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB: 9,
+	})
 
-	app := &App{router: router}
+	app := &App{router: router, rds: rds}
 
 	return app
 }
@@ -26,11 +34,41 @@ func (app *App) Start(ctx context.Context) error {
 		Handler: app.router,
 	}
 
-	err := server.ListenAndServe()
+	err := app.rds.Ping(ctx).Err()
 
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
 
-	return nil
+	defer func ()  {
+		if close_err := app.rds.Close(); close_err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	println("Connected to redis successfully!\nStarting server...")
+
+	ch := make(chan error, 1)
+
+	go func ()  {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+
+		close(ch)	
+	}()
+
+	// ch_err, open := <-ch
+	ch_err := <-ch
+
+	select {
+	case ch_err = <-ch:
+		return ch_err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
 }
--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
 
 	"github.com/sweatshirt2/go-analytics/application"
 )
 
 func main() {
-	println("Starting App...")
-
 	app := application.NewApp()
-	err := app.Start(context.TODO())
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	// err := app.Start(context.TODO())
+	err := app.Start(ctx)
 
 	// explicitly panicking because well... it's go
 	if err != nil {
